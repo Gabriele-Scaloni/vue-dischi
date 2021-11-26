3accacc4f7442e9a8114b4f050f56c5233641c17
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-11-26 17:02:48 +0100

    importato axios

--- src/components/MyCards.vue
+++ src/components/MyCards.vue
@@ -1,30 +1,11 @@
 <template>
 <div class="container-cards">
     <div class="cards">
-        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b7521c59-9c6d-4e12-8627-6411b1388bfb/darwvs5-0f4adcc4-77d5-426a-afe4-48f4798c5f7b.png/v1/fill/w_403,h_201,strp/_free__mix_and_match_pixel_bgs_by_sqdpxl_darwvs5-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjAxIiwicGF0aCI6IlwvZlwvYjc1MjFjNTktOWM2ZC00ZTEyLTg2MjctNjQxMWIxMzg4YmZiXC9kYXJ3dnM1LTBmNGFkY2M0LTc3ZDUtNDI2YS1hZmU0LTQ4ZjQ3OThjNWY3Yi5wbmciLCJ3aWR0aCI6Ijw9NDAzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.S3tdRtafIfskUSBdvw83FbhiElLGFU3hVknVwGFbofo" alt="album">
-        <h3>album323</h3>
-        <span>23</span>
-  </div>
-   <div class="cards">
-        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b7521c59-9c6d-4e12-8627-6411b1388bfb/darwvs5-0f4adcc4-77d5-426a-afe4-48f4798c5f7b.png/v1/fill/w_403,h_201,strp/_free__mix_and_match_pixel_bgs_by_sqdpxl_darwvs5-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjAxIiwicGF0aCI6IlwvZlwvYjc1MjFjNTktOWM2ZC00ZTEyLTg2MjctNjQxMWIxMzg4YmZiXC9kYXJ3dnM1LTBmNGFkY2M0LTc3ZDUtNDI2YS1hZmU0LTQ4ZjQ3OThjNWY3Yi5wbmciLCJ3aWR0aCI6Ijw9NDAzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.S3tdRtafIfskUSBdvw83FbhiElLGFU3hVknVwGFbofo" alt="album">
-        <h3>album323</h3>
-        <span>23</span>
-  </div>
-   <div class="cards">
-        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b7521c59-9c6d-4e12-8627-6411b1388bfb/darwvs5-0f4adcc4-77d5-426a-afe4-48f4798c5f7b.png/v1/fill/w_403,h_201,strp/_free__mix_and_match_pixel_bgs_by_sqdpxl_darwvs5-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjAxIiwicGF0aCI6IlwvZlwvYjc1MjFjNTktOWM2ZC00ZTEyLTg2MjctNjQxMWIxMzg4YmZiXC9kYXJ3dnM1LTBmNGFkY2M0LTc3ZDUtNDI2YS1hZmU0LTQ4ZjQ3OThjNWY3Yi5wbmciLCJ3aWR0aCI6Ijw9NDAzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.S3tdRtafIfskUSBdvw83FbhiElLGFU3hVknVwGFbofo" alt="album">
-        <h3>album323</h3>
-        <span>23</span>
-  </div>
-   <div class="cards">
-        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b7521c59-9c6d-4e12-8627-6411b1388bfb/darwvs5-0f4adcc4-77d5-426a-afe4-48f4798c5f7b.png/v1/fill/w_403,h_201,strp/_free__mix_and_match_pixel_bgs_by_sqdpxl_darwvs5-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjAxIiwicGF0aCI6IlwvZlwvYjc1MjFjNTktOWM2ZC00ZTEyLTg2MjctNjQxMWIxMzg4YmZiXC9kYXJ3dnM1LTBmNGFkY2M0LTc3ZDUtNDI2YS1hZmU0LTQ4ZjQ3OThjNWY3Yi5wbmciLCJ3aWR0aCI6Ijw9NDAzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.S3tdRtafIfskUSBdvw83FbhiElLGFU3hVknVwGFbofo" alt="album">
-        <h3>album323</h3>
-        <span>23</span>
-  </div>
-   <div class="cards">
-        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b7521c59-9c6d-4e12-8627-6411b1388bfb/darwvs5-0f4adcc4-77d5-426a-afe4-48f4798c5f7b.png/v1/fill/w_403,h_201,strp/_free__mix_and_match_pixel_bgs_by_sqdpxl_darwvs5-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjAxIiwicGF0aCI6IlwvZlwvYjc1MjFjNTktOWM2ZC00ZTEyLTg2MjctNjQxMWIxMzg4YmZiXC9kYXJ3dnM1LTBmNGFkY2M0LTc3ZDUtNDI2YS1hZmU0LTQ4ZjQ3OThjNWY3Yi5wbmciLCJ3aWR0aCI6Ijw9NDAzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.S3tdRtafIfskUSBdvw83FbhiElLGFU3hVknVwGFbofo" alt="album">
-        <h3>album323</h3>
-        <span>23</span>
-  </div>
+        <img :src="details.poster" alt="album">
+        <h3>{{details.title}}</h3>
+        <span>{{details.author}}</span>
+        <span>{{details.year}}</span>
+    </div>
 </div>
 </template>
 
@@ -32,7 +13,7 @@
 export default {
   name: 'MyMain',
   props: {
-    msg: String
+    details: Object,
   }
 }
 </script>
@@ -58,7 +39,6 @@ img {
   width: 100px;
   height: 100px;
   object-fit: cover;
- 
 }
 h3 {
   margin: 10px auto;
--- src/components/Principale.vue
+++ src/components/Principale.vue
@@ -3,12 +3,18 @@
 <div class="container">
   <div id="navbar"> </div>
  
-  <MyCards/>
+  <MyCards v-for="album in albums" 
+   :key="album.id"
+   :details="album" 
+   />
  
 </div>
 </template>
 
 <script>
+//importo axios e lo installo del terminale, prima uscendo dal progetto
+import axios from 'axios';
+//importo qui MyCards (component figlio di questo componente)
 import MyCards from '@/components/MyCards.vue'
 
 
@@ -16,8 +22,27 @@ export default {
   name: 'Principale',
   components: {
     MyCards
+  },
+  data() {
+    return {//inserire in apiUrl l'url dell'array
+      apiUrl :"https://flynn.boolean.careers/exercises/api/array/music",
+      albums: [],
+    }
+  },
+  created() {
+    this.getMyCards();
+  },
+    methods :{
+      getMyCards(){
+       axios
+      .get(this.apiUrl)
+      .then((result) => {
+        this.albums = result.data;
+      }
+      )}
+    }
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -32,5 +57,4 @@ export default {
   width:100%;
   height: 100%;
 }
-
 </style>
